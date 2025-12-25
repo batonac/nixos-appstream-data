c76aa114a9d95f2b0f172b7a16656c196a791b56
Kevin Shenk <kevin@avu.nu>
2025-12-25 03:53:03 +0000

    fmt: format flake

--- flake.nix
+++ flake.nix
@@ -6,11 +6,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in rec {
-      packages.nixos-appstream-data = pkgs.callPackage ./default.nix {};
-      packages.default = packages.nixos-appstream-data;
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages.nixos-appstream-data = pkgs.callPackage ./default.nix { };
+        packages.default = packages.nixos-appstream-data;
+      }
+    );
 }
